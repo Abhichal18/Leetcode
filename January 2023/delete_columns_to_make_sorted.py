7d092e8c8eda7e0a38399446c8e58b24c19c2d0e
Abhichal Rajput <mr.abhichalrajput@gmail.com>
2023-01-03 22:18:08 +0530

    Create delete_columns_to_make_sorted.py

---
+++ January 2023/delete_columns_to_make_sorted.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        # store removed column count
+        count=0
+        # iterating the 2-D array columnwise
+        for col in range(len(strs[0])):
+            for row in range(1,len(strs)):
+                # if current character is less than the character at previous position
+                # means the column is not sorted lexicographically
+                # hence, increment the count by 1
+                if strs[row][col]<strs[row-1][col]:
+                    count+=1
+                    # no meaning to check for further position so breaking out
+                    break
+        return count
